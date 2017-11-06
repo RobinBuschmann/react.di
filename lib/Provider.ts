0232323b2e2f29980ed4b0026851836d74ec683e
RobinBuschmann <robinbuschmann@hotmail.de>
2017-11-06 19:53:29 +0100

    first iteration

--- lib/Provider.ts
+++ lib/Provider.ts
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import {Component} from 'react';
+import {Component, ReactElement} from 'react';
 import {Container} from 'inversify';
 import {object} from 'prop-types';
+
+const isReact16Plus = parseFloat(React.version) >= 16;
 
 export interface ProviderProps {
   container: Container;
@@ -10,6 +12,14 @@ export interface ProviderProps {
 export class Provider extends Component<ProviderProps> {
   static childContextTypes = {container: object.isRequired};
   static contextTypes = {container: object};
+
+  constructor(props, context) {
+    super(props, context);
+
+    if (!isReact16Plus) {
+      this.render = () => React.Children.only(this.props.children) as any;
+    }
+  }
 
   componentWillReceiveProps() {
     // tslint:disable:no-console
@@ -29,6 +39,8 @@ export class Provider extends Component<ProviderProps> {
   }
 
   render() {
-    return React.Children.only(this.props.children);
+    return React.Children
+      .toArray(this.props.children)
+      .map((child, index) => React.cloneElement(child as ReactElement<any>, {key: index}));
   }
 }
